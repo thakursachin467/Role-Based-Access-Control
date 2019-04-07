58b2d29f756583bd369d76deaf741cdf39e36c2b
Sachin Thakur <thakursachin467@gmail.com>
2019-04-07 20:59:44 +0530

    more dummy routes added

--- Config/Credintials/Keys.js
+++
@@ -1,9 +0,0 @@
-const Keys= {
-    Sentrydsn: 'https://535debf6ec194c819209de8c5ddba337@sentry.io/1432934',
-    SecretOrKey:'ThisisSecretKey',
-    defaultRole:'STANDARD',
-    defaultPermissions:'READ',
-    dbUrl:'mongodb://admin:admin12@ds133556.mlab.com:33556/role-base-auth'
-};
-
-module.exports= Keys;
