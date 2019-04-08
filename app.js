a5de65ee6cedb24fae2dd69a552f83ae091d5add
Sachin Thakur <thakursachin467@gmail.com>
2019-04-08 10:44:01 +0530

    Dummy routes modified

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
-const express= require('express');
+const express = require('express');
 const app = express();
-const mongoose= require('mongoose');
-const jwt= require('jsonwebtoken');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-const PORT= process.env.PORT || 5000;
-const auth= require('./routes/users/Auth');
-const permissions= require('./routes/users/Permissions');
-const role= require('./routes/users/role');
+const PORT = process.env.PORT || 5000;
+const auth = require('./routes/users/Auth');
+const permissions = require('./routes/users/Permissions');
+const role = require('./routes/users/role');
 const Sentry = require('@sentry/node');
-const allRoutes= require('./routes/dummyRoutes/allRoutes');
-const access_rules= require('./Permissions/access_rules');
+const allRoutes = require('./routes/dummyRoutes/allRoutes');
+const access_rules = require('./Permissions/access_rules');
 let Keys;
-if(process.env.ENVIRONMENT ==='PRODUCTION'){
-    Keys= process.env
-} else{
-    Keys=  require('./Config/Credintials/keys');
+if (process.env.ENVIRONMENT === 'PRODUCTION') {
+    Keys = process.env
+} else {
+    Keys = require('./Config/Credintials/keys');
 }
 
 //basic middleware to check weather the user is logged in or not
 // if logged in attach the user id to request
- app.use((request,response,next)=>{
-    const authHeader= request.get('Authorization');
-    if(authHeader){
-        const token= authHeader.split(' ')[1];
-        const verification= jwt.verify(token,Keys.SecretOrKey);
-        request.userId= verification.userId;
+app.use((request, response, next) => {
+    const authHeader = request.get('Authorization');
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        const verification = jwt.verify(token, Keys.SecretOrKey);
+        request.userId = verification.userId;
     }
     next()
 });
 
 
 //sentry for error capturing
-Sentry.init({ dsn:Keys.Sentrydsn });
+Sentry.init({ dsn: Keys.Sentrydsn });
 
 //body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-mongoose.connect(Keys.dbUrl,{ useNewUrlParser: true })
-    .then(()=>console.log(`connection to database success`))
-    .catch(((err)=>{
+mongoose.connect(Keys.dbUrl, { useNewUrlParser: true })
+    .then(() => console.log(`connection to database success`))
+    .catch(((err) => {
         Sentry.captureException(err);
         console.log(`connection failed to database ${err}`)
     }));
 
 
-app.get('/',(request,response,next)=>{
-   response.send('Every one can access this route. To know all routes go to /api/allroutes');
+app.get('/', (request, response, next) => {
+    response.send('Every one can access this route. To know all routes go to /api/allroutes');
 });
 
-
-app.use('/api/allroutes',allRoutes)
+//routes for all dummy endpoints
+app.use('/api/allroutes', allRoutes)
 
 //only super admin will be able to define what permission can be set i.e read write etc
-app.use('/api/permissions',access_rules.canAccessRole(['superadmin']),permissions);
+app.use('/api/permissions', access_rules.canAccessRole(['superadmin']), permissions);
 
 //only user who has read access will be able to add or edit roles
-app.use('/api/role',access_rules.canAccessPermissions(['read']),role);
-app.use('/api/auth/',auth);
+app.use('/api/role', access_rules.canAccessPermissions(['read']), role);
+app.use('/api/auth/', auth);
 
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`APPLICATION STARTED ON PORT ${PORT}`);
 });
--- routes/dummyRoutes/allRoutes.js
+++ routes/dummyRoutes/allRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Permissions= require('../../Permissions/access_rules');
+const Permissions = require('../../Permissions/access_rules');
 
-router.get('/',(request,response,next)=>{
-   const text= `
+router.get('/', (request, response, next) => {
+    const text = `
    <p> You can create your account with /api/auth/signup with body structure as</p>
    <p> {
 	"email":"email",
@@ -30,37 +30,37 @@ router.get('/',(request,response,next)=>{
 
 <p>/api/allroutes/onlyAdmin. this route will only be accessable by admin</p>
 <p>/api/allroutes/onlyStandard. this route will only be accessed by standard user.</p>
-<p>/api/allroute/onlySuperUser. only accessed by superuser</p> 
-<p>/api/allroute/SuperUserAndStandard. anyone from superadmin or standard user can access this route.</p>
-<p>/api/allroute/SuperUserAndAdmin. anyone from superadmin or admin user can access this route.</p> 
-<p>/api/allroute/readAndWrite. anyone from read or write permission user can access this route.</p>
+<p>/api/allroutes/onlySuperUser. only accessed by superuser</p> 
+<p>/api/allroutes/SuperUserAndStandard. anyone from superadmin or standard user can access this route.</p>
+<p>/api/allroutes/SuperUserAndAdmin. anyone from superadmin or admin user can access this route.</p> 
+<p>/api/allroutes/readAndWrite. anyone from read or write permission user can access this route.</p>
    `;
     response.send(text);
 });
 
 
-router.get('/onlyAdmin',Permissions.canAccessRole(['admin']),(request,response,next)=>{
+router.get('/onlyAdmin', Permissions.canAccessRole(['admin']), (request, response, next) => {
     response.send('You are admin');
 });
 
-router.get('/onlyStandard',Permissions.canAccessRole(['standard']),(request,response,next)=>{
+router.get('/onlyStandard', Permissions.canAccessRole(['standard']), (request, response, next) => {
     response.send('You are standard user');
 });
 
-router.get('/onlySuperUser',Permissions.canAccessRole(['superadmin']),(request,response,next)=>{
+router.get('/onlySuperUser', Permissions.canAccessRole(['superadmin']), (request, response, next) => {
     response.send('You are Super admin');
 });
 
-router.get('/SuperUserAndStandard',Permissions.canAccessRole(['superadmin','standard']),(request,response,next)=>{
+router.get('/SuperUserAndStandard', Permissions.canAccessRole(['superadmin', 'standard']), (request, response, next) => {
     response.send('You are Super admin or standard user');
 });
 
-router.get('/SuperUserAndAdmin',Permissions.canAccessRole(['superadmin','admin']),(request,response,next)=>{
+router.get('/SuperUserAndAdmin', Permissions.canAccessRole(['superadmin', 'admin']), (request, response, next) => {
     response.send('You are Super admin or admin user');
 });
 
-router.get('/readAndWrite',Permissions.canAccessPermissions(['read','write']),(request,response,next)=>{
+router.get('/readAndWrite', Permissions.canAccessPermissions(['read', 'write']), (request, response, next) => {
     response.send('You have read  or write permissions');
 });
 
-module.exports= router;
+module.exports = router;
